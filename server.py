cee0167e3dad1d6141350663d12e7d2bda6a1593
routefleeder <114300403+routefleeder@users.noreply.github.com>
2025-02-25 19:01:08 +0200

    Update server.py

--- server.py
+++ server.py
@@ -7,6 +7,3 @@ app = FastAPI()
 def notify():
     print("Получена команда /arpem")
     return {"status": "Message received"}
-
-if __name__ == "__main__":
-    uvicorn.run(app, host="127.0.0.1", port=5000)
