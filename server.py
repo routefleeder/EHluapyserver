f194f4326ca881083825fffc59221fb314133887
routefleeder <114300403+routefleeder@users.noreply.github.com>
2025-03-01 21:58:22 +0200

    Update server.py

--- server.py
+++ server.py
@@ -5,6 +5,7 @@ import asyncio
 
 app = FastAPI()
 
+messages = []
 waiting_clients = []
 lock = asyncio.Lock()
 
@@ -16,11 +17,13 @@ class ChatMessage(BaseModel):
 @app.post("/api/notify")
 async def send_message(msg: ChatMessage):
     async with lock:
+        messages.append(msg)
         if waiting_clients:
             print(f"Новое сообщение от {msg.username}: {msg.text}. Отправляем ожидающим клиентам...")
             for username, client_queue in waiting_clients[:]:
                 if username != msg.username:
-                    await client_queue.put([msg])
+                    await client_queue.put([messages[:]])
+            messages.clear()
 
     return {"status": "ok"}
 
@@ -29,6 +32,12 @@ async def get_messages(username: str):
     """ Клиент ждёт новые сообщения. Сервер не отвечает сразу, если их нет. """
     my_queue = asyncio.Queue()
     async with lock:
+        pending_messages = [msg for msg in messages if msg.username != username]
+        if pending_messages:
+            print(f"Клиент {username} подключился, сразу отправляем ему {len(pending_messages)} сообщений.")
+            messages.clear()
+            return pending_messages
+        
         waiting_clients.append((username, my_queue))
 
     try:
