5ec8229f708a7b9506efb2316daee7f9d856c17e
routefleeder <114300403+routefleeder@users.noreply.github.com>
2025-03-03 13:35:41 +0200

    Update server.py

--- server.py
+++ server.py
@@ -19,18 +19,22 @@ async def websocket_endpoint(websocket: WebSocket):
             if not message:
                 continue
 
+            print(f"Received message: {message}")
+
             if message == "deactivate":
                 if active_sender == websocket:
                     active_sender = None
                     await websocket.send_text("deactivated")
                 continue
 
+            print(f"Checking if active_sender is None or if it is {active_sender} and not {websocket}")
             if active_sender is not None and active_sender != websocket:
                 await websocket.send_text("wait_for_deactivation")
                 print("Sent 'wait_for_deactivation' to ", websocket)
                 continue
 
             active_sender = websocket
+            print(f"Setting active_sender to {websocket}")
 
             for client in active_clients:
                 await client.send_text(message)
