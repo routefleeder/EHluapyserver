e880e2c04b356bd85b9dd675152a2be580baec46
routefleeder <114300403+routefleeder@users.noreply.github.com>
2025-03-03 11:19:31 +0200

    Update server.py

--- server.py
+++ server.py
@@ -2,24 +2,38 @@ from fastapi import FastAPI, WebSocket, WebSocketDisconnect
 
 app = FastAPI()
 
-# Список подключенных клиентов
-active_clients = []
+active_clients = set()
+active_sender = None
 
 
 @app.websocket("/ws")
 async def websocket_endpoint(websocket: WebSocket):
     await websocket.accept()
-    active_clients.append(websocket)
+    active_clients.add(websocket)
 
     try:
         while True:
-            # Ждем строку от клиента
             message = await websocket.receive_text()
 
-            # Рассылаем её всем подключенным клиентам
+            global active_sender
+
+            if message == "deactivate":
+                if active_sender == websocket:
+                    active_sender = None
+                    await websocket.send_text("deactivated")
+                continue
+
+            if active_sender and active_sender != websocket:
+                await websocket.send_text("wait_for_deactivation")
+                continue
+
+            active_sender = websocket
+
             for client in active_clients:
                 await client.send_text(message)
 
     except WebSocketDisconnect:
-        active_clients.remove(websocket)
+        active_clients.discard(websocket)
+        if active_sender == websocket:
+            active_sender = None
         print(f"Клиент отключился")
